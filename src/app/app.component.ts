54afc5feaad03daa65f689c1bd71af91e13bee1a
Sheimax <101610260+Sheimax@users.noreply.github.com>
2022-04-05 21:35:12 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Pedido } from './pedido';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Proyect_ALPROSUR';
+  
+  public pedido: Pedido = this.pedidoEmpty();
+  public pedidos: Pedido[] = [];
+
+  onDatos(): void {
+    console.log(this.pedido);
+    this.pedidos.push(this.pedido);
+    this.pedido = this.pedidoEmpty();
+  }
+
+  pedidoEmpty(): Pedido {
+    return {
+      supermercado: "",
+      producto: "",
+      cantidad: "",
+      fecha: ""
+    };
+  }
+
+  onDelete(i: number): void {
+    this.pedidos.splice(i, 1);
+  }
 }
